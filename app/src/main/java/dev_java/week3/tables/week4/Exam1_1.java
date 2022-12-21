e9ced416716679675fe158ba449bd5d12de14da7
eunyoungko <eunyoungko0529@gmail.com>
2022-12-21 17:48:53 +0900

    20221221

---
+++ app/src/main/java/dev_java/week3/tables/week4/Exam1_1.java
@@ -0,0 +1,13 @@
+package dev_java.week3.tables.week4;
+
+public class Exam1_1 {
+  public static void main(String[] args) {
+    String[] sArr = new String[] { "사과", "딸기", "오렌지" };
+
+    System.out.println(sArr[0]);
+    System.out.println(sArr[1]);
+    System.out.println(sArr[2]);
+
+  }
+
+}
---
+++ app/src/main/java/dev_java/week3/tables/week4/Exam1_3.java
@@ -0,0 +1,10 @@
+package dev_java.week3.tables.week4;
+
+public class Exam1_3 {
+  public static void main(String[] args) {
+    String[] sArr = new String[] { "빨강", "노랑", "파랑" };
+    for (int i = 0; i < sArr.length; i++) {
+      System.out.print(sArr[i] + " ");
+    }
+  }
+}
